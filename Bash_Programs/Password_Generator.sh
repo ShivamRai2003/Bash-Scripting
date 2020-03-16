a1cf08c2ad77c5ddf434c65ae8343481e9de59b0
Lucifer <srai14429467@gmail.com>
2020-03-16 20:54:46 +0530

    Update Password_Generator.sh

--- Bash_Programs/Password_Generator.sh
+++ Bash_Programs/Password_Generator.sh
@@ -3,5 +3,14 @@
 #!/bin/bash
 
 echo "Welcome to Password_Generator Programs !"
+
 echo "Kindly Enter the length of the password you want !"
+
 read Passwd_length
+
+for i in $(seq 1 5);
+
+do
+    openssl rand -base64 48 | cut -c1-$Passwd_length
+    
+done
