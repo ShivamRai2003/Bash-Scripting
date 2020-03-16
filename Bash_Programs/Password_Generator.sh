5199908f7028d8f3322714b379e8479cb60e258a
Lucifer <srai14429467@gmail.com>
2020-03-16 20:48:33 +0530

    Create Password_Generator.sh

---
+++ Bash_Programs/Password_Generator.sh
@@ -0,0 +1,7 @@
+# Hi Guys Here is the program for Password Generator !@!!
+
+#!/bin/bash
+
+echo "Welcome to Password_Generator Programs !"
+echo "Kindly Enter the length of the password you want !"
+read Passwd_length
